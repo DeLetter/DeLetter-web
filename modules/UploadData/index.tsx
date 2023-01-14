7a49b3d6e0b3e7263ab0e64d724a570de48e3800
timothyshen <timothyshen1@hotmail.com>
2023-01-14 22:17:23 +0000

    feat: update frontend UI

--- modules/UploadData/index.tsx
+++ modules/UploadData/index.tsx
@@ -1,16 +1,20 @@
 import { useCallback, useState } from 'react'
-import { useForm } from "react-hook-form";
-import { bundlrStore } from '@store/Bundlr';
+import { useForm } from 'react-hook-form'
+import { bundlrStore } from '@store/Bundlr'
 import { connectContract } from '@utils/contracts'
 import { Encryption } from '@utils/AES/encryption'
 import { UploadResponse } from '@bundlr-network/client/build/common/types'
 
 //Store on Arweave and write related data on blockchain
 const UploadData: React.FC = () => {
-  const { register, handleSubmit: withForm, formState: { errors } } = useForm()
+  const {
+    register,
+    handleSubmit: withForm,
+    formState: { errors },
+  } = useForm()
   const [hash, setHash] = useState('')
 
-  const bundlrInstance = bundlrStore.getState().bundlrInstance;
+  const bundlrInstance = bundlrStore.getState().bundlrInstance
 
   const uploadFile = useCallback(
     async (data: string) => {
@@ -47,53 +51,63 @@ const UploadData: React.FC = () => {
     [bundlrInstance]
   )
 
-  const handleSubmit = useCallback(withForm(async (data) => {
-    try {
-      const { lists, password } = data;
-      const encrypted = Encryption.encrypt(lists, password);
-      console.log('encryptedData', encrypted)
-      const tx = await uploadFile(encrypted)
-      console.log(tx)
-      const ftx = await writeArweaveAdd(tx)
-      console.log('ftx', ftx)
-      setHash(ftx.hash)
-      alert(`sucessfully stored on arweave and blockchain, hash: ${ftx.hash}`)
-    } catch (err) {
-      alert('something went wrong')
-    }
-  }), [bundlrInstance])
+  const handleSubmit = useCallback(
+    withForm(async (data) => {
+      try {
+        const { lists, password } = data
+        const encrypted = Encryption.encrypt(lists, password)
+        console.log('encryptedData', encrypted)
+        const tx = await uploadFile(encrypted)
+        console.log(tx)
+        const ftx = await writeArweaveAdd(tx)
+        console.log('ftx', ftx)
+        setHash(ftx.hash)
+        alert(`sucessfully stored on arweave and blockchain, hash: ${ftx.hash}`)
+      } catch (err) {
+        alert('something went wrong')
+      }
+    }),
+    [bundlrInstance]
+  )
   // TODO: add error notification
   return (
     <form onSubmit={handleSubmit}>
       <div className="mb-[14px] flex flex-col">
-        <label htmlFor="lists">Please enter you email list(Please, separate each item with semicolon)</label>
+        <label htmlFor="lists">
+          Please enter you email list(Please, separate each item with semicolon)
+        </label>
+        <p className="mb-2">Data format: Name Email</p>
         <textarea
-          placeholder='ricy@deletter.com; timtimtim@deletter.com;'
+          placeholder="tim timtimtim@deletter.com;"
           rows={4}
           id="lists"
-          {...register("lists", { required: true })}
+          {...register('lists', { required: true })}
           className="border-2"
         />
-        {errors.lists && <div className='text-red-500'>This field is required</div>}
+        {errors.lists && (
+          <div className="text-red-500">This field is required</div>
+        )}
       </div>
       <div className="mb-[14px] flex flex-col">
-        <label htmlFor="password">Set your password: </label>
+        <label className=" text-lg" htmlFor="password">
+          Set your password:{' '}
+        </label>
         <input
           type="password"
           id="password"
-          {...register("password", { required: true })}
+          {...register('password', { required: true })}
           className="border-2"
         />
-        {errors.password && <div className='text-red-500'>This field is required</div>}
+        {errors.password && (
+          <div className="text-red-500">This field is required</div>
+        )}
       </div>
-      <button
-        className="mb-[14px] border-2 p-2 border-black"
-      >
+      <button className="my-[16px] border-2 border-black p-2 items-center rounded-md hover:bg-black hover:text-white transition duration-300">
         Already entered all the email data and is quite aware of the password.
         Now it’s time to encrypt these data and store in Arweave!
       </button>
-      <h2>Hash : </h2>
-      {hash && <p className='text-green-500 break-all'>{hash}</p>}
+      <h3 className="text-lg font-bold">Hash : </h3>
+      {hash && <p className="text-green-500 break-all">{hash}</p>}
     </form>
   )
 }

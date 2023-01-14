e2f6fc9119bbc6fcd15f43ba1acb43a0cc23b754
timothyshen <timothyshen1@hotmail.com>
2023-01-14 22:17:23 +0000

    feat: update frontend UI

--- modules/LoadData/index.tsx
+++ modules/LoadData/index.tsx
@@ -1,45 +1,67 @@
 import { useCallback, useState } from 'react'
-import { useForm } from "react-hook-form";
+import { useForm } from 'react-hook-form'
 import { Encryption } from '@utils/AES/encryption'
 import { bundlrStore } from '@store/Bundlr'
 import { getAddress } from 'services/readAreave'
 
 const LoadData: React.FC = () => {
   const [loadedData, setLoadedData] = useState('')
-  const { register, handleSubmit: withForm, formState: { errors } } = useForm()
-  const bundlrInstance = bundlrStore.getState().bundlrInstance;
+  const {
+    register,
+    handleSubmit: withForm,
+    formState: { errors },
+  } = useForm()
+  const bundlrInstance = bundlrStore.getState().bundlrInstance
 
-  const handleSubmit = useCallback(withForm(async (data) => {
-    const { enteredpassword } = data;
-    const arId = await getAddress()
-    if (!arId) { console.log('arId is undefined'); return; }
-    const res = await fetch(`https://arweave.net/${arId}`)
-    const arweaveRes = await res.json()
-    const decryptedData = await Encryption.decrypt(arweaveRes, enteredpassword)
-    setLoadedData(JSON.stringify(decryptedData));
-  }), [bundlrInstance])
+  const handleSubmit = useCallback(
+    withForm(async (data) => {
+      const { enteredpassword } = data
+      const arId = await getAddress()
+      if (!arId) {
+        console.log('arId is undefined')
+        return
+      }
+      const res = await fetch(`https://arweave.net/${arId}`)
+      const arweaveRes = await res.json()
+      const decryptedData = await Encryption.decrypt(
+        arweaveRes,
+        enteredpassword
+      )
+      setLoadedData(JSON.stringify(decryptedData))
+    }),
+    [bundlrInstance]
+  )
 
   return (
-    <div className='flex flex-col justify-between'>
-      <form onSubmit={handleSubmit} className='mb-[19px]'>
+    <div className="flex flex-col justify-between">
+      <form onSubmit={handleSubmit} className="mb-[19px]">
         <div className="mb-[14px] flex flex-col">
           <label htmlFor="password">Enter your password: </label>
           <input
             type="password"
             id="enteredpassword"
-            {...register("enteredpassword", { required: true })}
+            {...register('enteredpassword', { required: true })}
             className="border-2"
           />
-          {errors.enteredpassword && <div className='text-red-500'>This field is required</div>}
+          {errors.enteredpassword && (
+            <div className="text-red-500">This field is required</div>
+          )}
         </div>
-        <button className="w-full border-2 border-black" >
+        <button className="w-full my-[8px] border-2 border-black p-2 items-center rounded-md hover:bg-black hover:text-white transition duration-300">
           Load data from arweave
         </button>
       </form>
-      <div className='flex-auto border-[2px] border-black border-dashed'>{loadedData}</div>
-      <a className='mt-[19px] flex justify-center items-center w-full border-2 border-black' href={`mailto:${loadedData}`} >Send emails</a>
+      <div className="flex-auto border-[2px] border-black border-dashed">
+        {loadedData}
+      </div>
+      <a
+        className="mt-[19px] flex justify-center items-center w-full border-2 border-black rounded-md hover:bg-black hover:text-white transition duration-300"
+        href={`mailto:${loadedData}`}
+      >
+        Send emails
+      </a>
     </div>
   )
 }
 
-export default LoadData;
+export default LoadData
--- pages/index.tsx
+++ pages/index.tsx
@@ -29,7 +29,9 @@ export default function Home() {
   }, [bundlrInstance, fetchBalance])
 
   function parseIntToEther(amount: number) {
-    const conv = (BigNumber.from(amount)).mul(bundlrInstance!.currencyConfig.base[1].toString())
+    const conv = BigNumber.from(amount).mul(
+      bundlrInstance!.currencyConfig.base[1].toString()
+    )
     return conv
   }
 
@@ -37,27 +39,64 @@ export default function Home() {
     <>
       <Head>
         <title>DeLetter</title>
-        <meta name="description" content="A decentralized contacts management tool" />
+        <meta
+          name="description"
+          content="A decentralized contacts management tool"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex flex-col items-center">
         <div className="w-4/5">
-          <div className='flex justify-center items-center h-[100px] font-semibold text-[16px]'>
-            DeLetter's still developing. Many errors may occur, please pay attention to your metamask transaction status and console
+          <div className="m-3 w-full flex justify-center bg-grey-100">
+            <div className="font-bold text-[16px] bg-gray-200 p-5 text-red-500">
+              DeLetter's still developing. Many errors may occur, please pay
+              attention to your metamask transaction status and console!
+              <div className="text-center">
+                For any bug please use this form: <br />
+                <a
+                  href="https://forms.gle/qPwTjT4RLDqoGMJ49"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Bug Reporting
+                </a>
+              </div>
+            </div>
           </div>
+          {/* <h1 className="mb-4 text-4xl font-extrabold text-center leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl my-[24px]">
+            DeLetter
+          </h1> */}
+
           <ul>
-            <li>Please first have a metamask, then change the net to goerli network</li>
+            <li>
+              Please first have a metamask, then change the net to goerli
+              network
+            </li>
             <li>If you don't have funds on bundlr,please fund first</li>
-            <li>Enter your contacts’ emails and then enter a password used to encrypt your contacts’ emails data</li>
-            <li>When retrieving your data, please enter your password used for encryption first, then click `Load my data` button. </li>
-            <li>Click `Send Emails` button, then it will trigger the default email client application in your laptop.</li>
+            <li>
+              Enter your contacts’ emails and then enter a password used to
+              encrypt your contacts’ emails data
+            </li>
+            <li>
+              When retrieving your data, please enter your password used for
+              encryption first, then click `Load my data` button.{' '}
+            </li>
+            <li>
+              Click `Send Emails` button, then it will trigger the default email
+              client application in your laptop.
+            </li>
           </ul>
-          <div className='grid grid-cols-1 md:grid-cols-2 gap-[10px] mt-[24px]'>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-[20px] mt-[24px]">
             <div>
-              <h3 >Balance: {balance}</h3>
-              <button className="mb-[24px] border-2 border-black" onClick={fundWallet}>
-                Fund 0.1 goerli eth in Wallet
+              <h3 className="text-lg font-bold">
+                Balance: <span className="text-indigo-700">{balance}</span>
+              </h3>
+              <button
+                className="my-[16px] border-2 border-black p-2 items-center rounded-md hover:bg-black hover:text-white transition duration-300"
+                onClick={fundWallet}
+              >
+                Fund <span className="font-bold">0.1</span> goerli eth in Wallet
               </button>
               <UploadData />
             </div>

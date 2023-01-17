bec6a54a539ff95f7a59806aa1d81c2516a58c5f
Ricy137 <1932353948@qq.com>
2023-01-17 12:15:56 +0800

    fix: landing button ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <div className='flex flex-col justify-center w-[60%] z-10'>
         <p className='text-center font-semibold text-[28px] leading-[36px]'>Manage your contacts' email lists, and really own the data</p>
         <p className='text-center mt-[14px] text-[16px]'>A decentralized email lists management tool, which ensure your ownership and privacy with blockchain,decentralized storage and cryptography technology</p>
-        <Link href='/management' className='p-2 mt-[14px] border-2 border-black hover:bg-[#000000] hover:text-[#ffffff] text-center'>Start manage </Link>
+        <Link href='/management' className='p-2 mt-[14px] border-2 border-black hover:bg-[#000000] hover:text-[#ffffff] text-center rounded-md'>Start manage </Link>
       </div>
     </div>
   )

993242e01786387bded4f8b0f58a925605503e87
Ricy137 <1932353948@qq.com>
2023-01-23 11:58:00 +0800

    fix: navigation style

--- modules/Navigation/index.tsx
+++ modules/Navigation/index.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const Navigation: React.FC = () => {
   return (
-    <nav className="sticky top-0 flex flex-row justify-center items-center h-[64px] text-gray-500">
+    <nav className="fixed top-0 flex flex-row justify-center items-center h-[64px] text-gray-500">
       <div className="w-4/5">
         <Link href="/upload">UploadData</Link>
         <Link href="/download" className="ml-[10px]">DownloadData</Link>

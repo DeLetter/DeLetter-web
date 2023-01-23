70c8582d4efcb84363f295466e633e7f25a64443
Ricy137 <1932353948@qq.com>
2023-01-23 22:57:44 +0800

    fix: balance problems

--- store/Bundlr.ts
+++ store/Bundlr.ts
@@ -1,6 +1,8 @@
 import create from 'zustand'
+import { subscribeWithSelector } from 'zustand/middleware'
 import { WebBundlr } from '@bundlr-network/client'
 import { providers, utils } from 'ethers'
+// import { debounce } from 'lodash-es';
 
 export interface BundlrStore {
   initialBundlr: () => Promise<void> | void
@@ -10,55 +12,57 @@ export interface BundlrStore {
   fetchBalance: () => Promise<void> | void
 }
 
-export const bundlrStore = create<BundlrStore>((set, get) => ({
-  bundlrInstance: null,
-  balance: '',
-  account: '',
-  initialBundlr: async () => {
-    try {
-      if (!window?.ethereum) {
-        alert('please install metamask')
-        return
+export const bundlrStore = create(
+  subscribeWithSelector<BundlrStore>((set, get) => ({
+    bundlrInstance: null,
+    balance: '',
+    account: '',
+    initialBundlr: async () => {
+      try {
+        if (!window?.ethereum) {
+          alert('please install metamask')
+          return
+        }
+        const addresses = await window.ethereum.request!({
+          method: 'eth_requestAccounts',
+        })
+        //TODO: seperate account and bundlr storage, they're not supposed to be the same storage
+        set({ account: addresses[0] })
+        const provider = new providers.Web3Provider(window.ethereum)
+        await provider._ready()
+        const chainId = await provider.getNetwork()
+        if (chainId.chainId != 5) {
+          alert('please switch to goerli network')
+          return
+        }
+        const bundlr = new WebBundlr(
+          'https://devnet.bundlr.network',
+          'ethereum',
+          provider,
+          { providerUrl: 'https://ethereum-goerli-rpc.allthatnode.com' }
+        )
+        await bundlr.ready()
+        set({ bundlrInstance: bundlr })
+      } catch (err) {
+        console.log(err)
+        debugger
+        alert('something went wrong')
       }
-      const addresses = await window.ethereum.request!({
-        method: 'eth_requestAccounts',
-      })
-      //TODO: seperate account and bundlr storage, they're not supposed to be the same storage
-      set({ account: addresses[0] })
-      const provider = new providers.Web3Provider(window.ethereum)
-      await provider._ready()
-      const chainId = await provider.getNetwork()
-      if (chainId.chainId != 5) {
-        alert('please switch to goerli network')
-        return
+    },
+    fetchBalance: async () => {
+      try {
+        const bundlr = get().bundlrInstance
+        if (bundlr) {
+          const balance = await bundlr.getLoadedBalance()
+          set({ balance: utils.formatEther(balance?.toString() ?? '0') })
+        }
+      } catch (err) {
+        console.log(err)
+        alert('something went wrong')
       }
-      const bundlr = new WebBundlr(
-        'https://devnet.bundlr.network',
-        'ethereum',
-        provider,
-        { providerUrl: 'https://ethereum-goerli-rpc.allthatnode.com' }
-      )
-      await bundlr.ready()
-      set({ bundlrInstance: bundlr })
-    } catch (err) {
-      console.log(err)
-      debugger
-      alert('something went wrong')
-    }
-  },
-  fetchBalance: async () => {
-    try {
-      const bundlr = get().bundlrInstance
-      if (bundlr) {
-        const balance = await bundlr.getLoadedBalance()
-        set({ balance: utils.formatEther(balance?.toString() ?? '0') })
-      }
-    } catch (err) {
-      console.log(err)
-      alert('something went wrong')
-    }
-  },
-}))
+    },
+  }))
+)
 
 export const useBundlrInstance = () =>
   bundlrStore((state) => state.bundlrInstance)
@@ -67,5 +71,14 @@ export const useInitializedBundlr = () =>
   bundlrStore((state) => state.initialBundlr)
 export const useFetchBundlrBalance = () =>
   bundlrStore((state) => state.fetchBalance)
-
 export const useAccount = () => bundlrStore((state) => state.account)
+
+// const updateBundlrBalance = async () => {
+//   const fetchBalance = bundlrStore.getState().fetchBalance
+//   await fetchBalance()
+// }
+bundlrStore.subscribe(
+  (state) => state.bundlrInstance,
+  bundlrStore.getState().fetchBalance,
+  { fireImmediately: true }
+)

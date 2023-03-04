cce2dd98e18eb82ad92018d5ebb75548651c4405
Ricy <1932353948@qq.com>
2023-03-04 18:17:00 +0800

    fix: send email ui

--- pages/download/EmailButton/index.tsx
+++ pages/download/EmailButton/index.tsx
@@ -1,6 +1,11 @@
-import React, { useState } from 'react'
+import React, { useCallback } from 'react'
 import Button from '@components/Button'
-import { SendEmail, handleSendingEmail } from '@services/EmailList'
+import {
+  SendEmail,
+  handleSendingEmail as _handleSendingEmail,
+} from '@services/EmailList'
+import cx from 'clsx'
+import useInTransaction from '@hooks/useInTransaction'
 import Error from 'next/error'
 
 const EmailSendingButton: React.FC<SendEmail> = ({
@@ -8,49 +13,26 @@ const EmailSendingButton: React.FC<SendEmail> = ({
   emailSubject,
   emailBody,
 }) => {
-  const [emailError, setEmailError] = useState(false)
-  const [emailErrorMessage, setEmailErrorMessage] = useState('')
-  const [emailLoading, setEmailLoading] = useState(false)
-  const [emailSent, setEmailSent] = useState(false)
-  const [emailSentMessage, setEmailSentMessage] = useState('')
+  const { inTransaction, execTransaction: handleSendingEmail } =
+    useInTransaction(_handleSendingEmail)
 
-  //TODO
-  const sendEmail = async () => {
+  const sendEmail = useCallback(async () => {
     try {
-      let res = await handleSendingEmail({ emailTo, emailSubject, emailBody })
+      await handleSendingEmail({ emailTo, emailSubject, emailBody })
     } catch (err) {
       console.log(err)
     }
-    // setEmailLoading(true)
-    // try {
-    //   const markdownEmail = htmlToMarkdown(emailBody)
-
-    //   const response = await fetch('/api/sendEmail', {
-    //     method: 'POST',
-    //     headers: {
-    //       'Content-Type': 'application/json',
-    //     },
-    //     body: JSON.stringify({ emailTo, emailSubject, markdownEmail }),
-    //   })
-    // } catch (err: Error | any) {
-    //   setEmailError(true)
-    //   console.log(err.message)
-    //   setEmailErrorMessage(err.message)
-    // } finally {
-    //   setEmailLoading(false)
-    //   setEmailSent(true)
-    // }
-  }
+  }, [emailTo, emailSubject, emailBody])
   return (
     <Button
       onClick={sendEmail}
-      className={
-        'w-full border-2 border-black p-2 items-center rounded-md hover:bg-black hover:text-white transition duration-300'
-      }
+      className={cx(
+        'w-full border-2 border-black p-2 items-center rounded-md hover:bg-black hover:text-white transition duration-300',
+        inTransaction && 'pointer-events-none opacity-30'
+      )}
     >
-      {emailLoading ? 'Sending...' : 'Send Email'}
-      {emailError && <Error statusCode={500} title={emailErrorMessage} />}
-      {emailSent && <div>Email sent successfully</div>}
+      {inTransaction ? 'Sending...' : 'Send Email'}
+      {/* {emailError && <Error statusCode={500} title={emailErrorMessage} />} */}
     </Button>
   )
 }

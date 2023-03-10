b0c1a3d75b71088c3429c0f732c9896026ef2998
Ricy <1932353948@qq.com>
2023-03-10 20:52:20 +0800

    fix: revert

--- utils/contracts/index.ts
+++ utils/contracts/index.ts
@@ -1,39 +1,24 @@
 import { ethers } from 'ethers'
 import abiJSON from './abi/base.json'
 import { BASE_CONTRACT_ADDRESS } from '../constants'
-import { fetchApi } from '@utils/fetch/fetchApi'
-import { Alchemy, AlchemyProvider, Network, Wallet } from 'alchemy-sdk'
-import Encryption from '@utils/AES/encryption'
+
 //TODO: Abstract
 export const connectContract = async () => {
   const contractABI = abiJSON
+  let baseContract: ethers.Contract
   try {
     const { ethereum } = window
     if (!ethereum || ethereum.chainId !== '0x5') {
       throw new Error('Please connect to the Goerli network.')
     }
-    const encrypt = await fetchApi({
-      path: 'configInfo',
-      method: 'GET',
-    })
-    const decrypt = await Encryption.decrypt(
-      encrypt,
-      process.env.NEXT_PUBLIC_AES_KEY as string
-    )
-    const { rpc, privateKey } = JSON.parse(decrypt)
-    const settings = {
-      apiKey: rpc,
-      network: Network.ETH_GOERLI, // Replace with your network.
-    }
-    const alchemy = new Alchemy(settings)
-    const etherProvider = await alchemy.config.getProvider()
-    const wallet = new Wallet(privateKey, etherProvider)
-    const baseContract = new ethers.Contract(
+    const provider = new ethers.providers.Web3Provider(ethereum)
+    const signer = provider.getSigner()
+    baseContract = new ethers.Contract(
       BASE_CONTRACT_ADDRESS as string,
       contractABI,
-      wallet
+      signer
     ) // instantiating new connection to the contract
-    return { baseContract, wallet }
+    return { baseContract, signer }
   } catch (error) {
     console.log('ERROR:', error)
     throw new Error('sorry something went wrong')
